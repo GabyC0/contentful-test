5610839c0957a95f319c40051181526ca2b579e9
Gabriela <74997368+GabyC0@users.noreply.github.com>
2025-04-17 11:08:23 -0700

    changed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // enables static export
+    trailingSlash: true, // optional, but good for static hosting consistency
+  }
+  
+  export default nextConfig
+  
