7f4c8f3c5a913cb229d28ab16ba42da6f26ab8c9
Gabriela <74997368+GabyC0@users.noreply.github.com>
2025-04-17 12:58:18 -0700

    added wrangler file

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,23 @@
 // export default nextConfig;
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export', // enables static export
+//     trailingSlash: true, // optional, but good for static hosting consistency
+//   }
+  
+//   export default nextConfig
+  
+
+//from cloudflare doc:
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // enables static export
-    trailingSlash: true, // optional, but good for static hosting consistency
-  }
-  
-  export default nextConfig
-  
+const nextConfig = {};
+
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+
+export default nextConfig;
